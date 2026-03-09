b392857bf5afb186602384acbd67d39bccfe4b0c
atxatlarge-code <atxatlarge@gmail.com>
2026-03-09 15:52:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Set page configuration
-st.set_page_config(page_title="Property Distress Scorer", layout="wide")
+st.set_page_config(page_title="Seller  Signal", layout="wide")
 
 st.title("🏘️ Seller Signal")
 st.markdown("""
